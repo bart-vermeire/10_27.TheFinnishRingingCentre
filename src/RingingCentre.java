f9e0fa5e75446802ad9917f5b48f5dc2a3cbe930
bart-vermeire <bart.vermeire.erni@gmail.com>
2017-07-21 17:58:07 +0200

    Initial commit

---
+++ src/RingingCentre.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+/**
+ * Created by veba on 21/07/2017.
+ */
+public class RingingCentre {
+    private HashMap<Bird, String> ringingCentre;
+    public RingingCentre(){
+
+        this.ringingCentre=new HashMap<Bird, String>();
+    }
+    public void observe(Bird bird, String place){
+
+    }
+    public void observations(Bird bird){
+
+    }
+}
